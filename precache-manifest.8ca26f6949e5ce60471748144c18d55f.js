873cd96676646beb459f91c8d1caf80cb8ef84b8
Sergiy Fartushniak <fartux97@gmail.com>
2021-07-20 20:50:32 +0300

    Updates

---
+++ precache-manifest.8ca26f6949e5ce60471748144c18d55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7278a4133e33e4c78134ccb44d81e4d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "324f4947050a59831b69",
+    "url": "./static/css/main.2a2df6c7.chunk.css"
+  },
+  {
+    "revision": "d36cf7d1584e4e69c9e6",
+    "url": "./static/js/2.0472d363.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0472d363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324f4947050a59831b69",
+    "url": "./static/js/main.4e1fcab0.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
